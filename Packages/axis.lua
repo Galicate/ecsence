a1eb2ad52d5c03e5e54c5de0d4bb5f9d08629f2d
Galicate <logankeast02@gmail.com>
2026-09-24 01:26:44 +0200

    Untrack files in .gitignore

--- Packages/axis.lua
+++
@@ -1,6 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["neond00m_axis@0.2.4"]["axis"])
-export type DeviceType = REQUIRED_MODULE.DeviceType 
-export type Map<T> = REQUIRED_MODULE.Map<T>
-export type Input<T> = REQUIRED_MODULE.Input<T>
-export type Axis = REQUIRED_MODULE.Axis 
-return REQUIRED_MODULE

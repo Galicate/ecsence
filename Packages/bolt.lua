32396ce43642efe245d9814307cd09c21c15c4ca
Galicate <logankeast02@gmail.com>
2026-09-24 01:20:55 +0200

    First!

---
+++ Packages/bolt.lua
@@ -0,0 +1,21 @@
+local REQUIRED_MODULE = require(script.Parent._Index["unityjaeger_bolt@1.0.2"]["bolt"])
+export type Shape = REQUIRED_MODULE.Shape 
+export type BoxShape = REQUIRED_MODULE.BoxShape 
+export type SphereShape = REQUIRED_MODULE.SphereShape 
+export type EllipsoidShape = REQUIRED_MODULE.EllipsoidShape 
+export type CapsuleShape = REQUIRED_MODULE.CapsuleShape 
+export type CylinderShape = REQUIRED_MODULE.CylinderShape 
+export type WedgeShape = REQUIRED_MODULE.WedgeShape 
+export type CornerWedgeShape = REQUIRED_MODULE.CornerWedgeShape 
+export type Child = REQUIRED_MODULE.Child 
+export type Composite = REQUIRED_MODULE.Composite 
+export type StaticComposite = REQUIRED_MODULE.StaticComposite 
+export type DynamicComposite = REQUIRED_MODULE.DynamicComposite 
+export type Hull = REQUIRED_MODULE.Hull 
+export type HullShape = REQUIRED_MODULE.HullShape 
+export type Support<T = Shape> = REQUIRED_MODULE.Support<T >
+export type Tree = REQUIRED_MODULE.Tree 
+export type DynamicTree = REQUIRED_MODULE.DynamicTree 
+export type StaticTree = REQUIRED_MODULE.StaticTree 
+export type DynamicTreeConfig = REQUIRED_MODULE.DynamicTreeConfig 
+return REQUIRED_MODULE
